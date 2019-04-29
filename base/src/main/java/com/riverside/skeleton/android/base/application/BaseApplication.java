882760b571f001b72caa68e7f602d0d315d27882
b_e <b_e@163.com>
2019-04-29 16:23:03 +0800

    初始化版本

---
+++ base/src/main/java/com/riverside/skeleton/android/base/application/BaseApplication.java
@@ -0,0 +1,73 @@
+package com.riverside.skeleton.android.base.application;
+
+import android.os.StrictMode;
+import android.support.multidex.MultiDexApplication;
+
+import com.riverside.skeleton.android.base.activity.ActivityStaticMonitor;
+import com.riverside.skeleton.android.base.utils.CollectInfo.AppVersionInfo;
+import com.riverside.skeleton.android.base.utils.CollectInfo.CollectInfoHelper;
+import com.riverside.skeleton.android.base.utils.CollectInfo.DisplayInfo;
+import com.riverside.skeleton.android.base.utils.CollectInfo.OSInfo;
+import com.riverside.skeleton.android.base.utils.CollectInfo.OSVersionInfo;
+import com.riverside.skeleton.android.base.utils.CollectInfo.TelephonyInfo;
+import com.riverside.skeleton.android.base.utils.CrashCallback;
+import com.riverside.skeleton.android.util.log.CLog;
+import com.zxy.recovery.core.Recovery;
+
+/**
+ * Application基类    1.0
+ * <p>
+ * b_e 2017/11/20
+ */
+public class BaseApplication extends MultiDexApplication {
+    private static BaseApplication instance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //保存Application单例对象
+        instance = this;
+
+        CLog.init(getApplicationContext());
+
+        //设置收集信息
+        CollectInfoHelper.init(instance);
+        CollectInfoHelper.addInfoSource(
+                AppVersionInfo.class
+                , OSInfo.class
+                , OSVersionInfo.class
+                , TelephonyInfo.class
+                , DisplayInfo.class
+        );
+
+        if (CLog.isDebug()) {
+            //开启Android的严格模式
+            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().detectAll().penaltyLog().build());
+            StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder().detectAll().penaltyLog().build());
+        } else {
+            CLog.setLogLevel(CLog.LEVEL_ERROR);
+        }
+
+        //启动防灾模式
+        Recovery.getInstance()
+                .debug(CLog.isDebug())
+                .recoverInBackground(true)
+                .recoverStack(CLog.isDebug())
+                .callback(new CrashCallback(getApplicationContext()))
+                .silent(true, Recovery.SilentMode.RESTART)
+                .init(this);
+
+        //启动Activity状态监控
+        ActivityStaticMonitor.init(this);
+    }
+
+    /**
+     * 取得Application单例对象
+     *
+     * @return
+     */
+    public static BaseApplication getInstance() {
+        return instance;
+    }
+}
